a43edee8299f0fcef4366ee0864d0dffefa49e7f
Tinus <vertelmy@gmail.com>
2016-06-01 15:44:00 +1200

    adeed and configured paperclip to show images.

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -41,7 +41,7 @@ class PinsController < ApplicationController
 	private
 
 	def pin_params
-		params.require(:pin).permit(:title, :desc)
+		params.require(:pin).permit(:title, :desc, :image)
 	end
 
 	def find_pin
